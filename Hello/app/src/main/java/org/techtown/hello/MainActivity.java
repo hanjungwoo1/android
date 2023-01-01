798dc77923683e956b3f8ab219eaba62485df94f
jungwoo <hanjungwoo1@naver.com>
2023-01-01 20:51:47 +0900

    FEAT : APP

--- Hello/app/src/main/java/org/techtown/hello/MainActivity.java
+++ Hello/app/src/main/java/org/techtown/hello/MainActivity.java
@@ -3,6 +3,8 @@ package org.techtown.hello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v){
+        Toast.makeText(this, "버튼을 눌렀습니다.", Toast.LENGTH_LONG).show();
     }
 }
